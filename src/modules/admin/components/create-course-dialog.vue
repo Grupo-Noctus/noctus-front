7d465bee822f7a9e6e00972879c079f12b6f1b3e
icarocaiquecaiq <icarocaiquevp16@gmail.com>
2025-06-01 18:42:42 -0300

    Fix: PR fixes

--- src/modules/admin/components/create-course-dialog.vue
+++ src/modules/admin/components/create-course-dialog.vue
@@ -57,7 +57,7 @@
                                 v-model="createCourseForm.imageCourse"
                                 accept="image/png, image/jpeg"
                                 label="imagem do curso"
-                                placeholder="carregue sua imagem"
+                                placeholder="Carregue sua imagem"
                                 prepend-icon="mdi-image-area"
                                 variant="outlined"
                             ></v-file-input>
